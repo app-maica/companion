9fa46b0c74311400e79bf5150868a59939e4a268
App Maica <maica.xyz@gmail.com>
2016-09-22 10:30:43 -0500

    Startup files

---
+++ webpack.development.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.base.config');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+webpackConfig.plugins.push(
+    //new ExtractTextPlugin('[name].css'),
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: 'development'
+        }
+    }),
+    new BrowserSyncPlugin({
+        host: 'localhost',
+        port: 3000,
+        server: {
+            baseDir: ['./www']
+        }
+    })
+);
+
+webpackConfig.devtool = 'source-map';
+
+module.exports = webpackConfig;
